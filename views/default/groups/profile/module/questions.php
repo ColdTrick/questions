2719bcf5ce776999ebcf10af2d5878a307fe38c8
Jerome Bakker <jeabakker@coldtrick.com>
2021-07-09 15:14:33 +0200

    chore: use more group module magic

--- views/default/groups/profile/module/questions.php
+++ views/default/groups/profile/module/questions.php
@@ -5,50 +5,12 @@
  * @package Questions
  */
 
-$group = elgg_extract('entity', $vars);
-if (!$group instanceof ElggGroup) {
-	return;
-}
-
-$all_link = elgg_view('output/url', [
-	'href' => elgg_generate_url('collection:object:question:group', [
-		'guid' => $group->guid,
-	]),
-	'text' => elgg_echo('link:view:all'),
-	'is_trusted' => true,
-]);
-
-elgg_push_context('widgets');
-
-$content = elgg_list_entities([
-	'type' => 'object',
-	'subtype' => ElggQuestion::SUBTYPE,
-	'container_guid' => $group->guid,
-	'limit' => 6,
-	'full_view' => false,
-	'pagination' => false,
-]);
-
-elgg_pop_context();
-
-if (!$content) {
-	$content = elgg_view('output/longtext', ['value' => elgg_echo('questions:none')]);
-}
-
-$new_link = '';
-if ($group->canWriteToContainer(0, 'object', ElggQuestion::SUBTYPE)) {
-	$new_link = elgg_view('output/url', [
-		'href' => elgg_generate_url('add:object:question', [
-			'guid' => $group->guid,
-		]),
-		'text' => elgg_echo('add:object:question'),
-		'is_trusted' => true,
-	]);
-}
-
-echo elgg_view('groups/profile/module', [
+$params = [
 	'title' => elgg_echo('questions:group'),
-	'content' => $content,
-	'all_link' => $all_link,
-	'add_link' => $new_link,
-]);
+	'entity_type' => 'object',
+	'entity_subtype' => ElggQuestion::SUBTYPE,
+	'no_results' => elgg_echo('questions:none'),
+];
+$params = $params + $vars;
+
+echo elgg_view('groups/profile/module', $params);
